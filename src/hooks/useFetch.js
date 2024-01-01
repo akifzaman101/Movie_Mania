278b1782cdd201b4b7eaf90b1d325fa8c86a6dd6
akifzaman101 <akifzaman101@yahoo.com>
2024-01-01 15:10:12 +0600

    ApiKeyUpdated

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,8 +1,9 @@
 import { useState,useEffect } from "react"
 
 export const useFetch = (api, queryTerm="") => {
+  const apiKey = process.env.REACT_APP_API_KEY;
     const[data,setData] = useState([]);
-    const url = `https://api.themoviedb.org/3/${api}?api_key=${process.env.REACT_APP_API_KEY}&query=${queryTerm}`;
+    const url = `https://api.themoviedb.org/3/${api}?api_key=${apiKey}&query=${queryTerm}`;
 
     useEffect(() => {
         async function fetchMovies(){
